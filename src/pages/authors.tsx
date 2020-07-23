9d1ec69f88215ea6227ddc927b9ea099acdd04aa
tanigaki <junya.tanigaki.39@gmail.com>
2020-07-23 10:24:22 +0900

    chore: Use useStaticQuery

--- src/pages/authors.tsx
+++ src/pages/authors.tsx
@@ -1,43 +1,39 @@
 import * as React from 'react'
-import { Link, graphql } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import { AuthorsQuery } from '../../types/graphql-types'
 // ______________________________________________________
 //
-type Props = {
-  data: AuthorsQuery
-}
-// ______________________________________________________
-//
-const Component: React.FC<Props> = ({ data }) => (
-  <div>
-    <h1>Authors</h1>
-    {data.site?.siteMetadata?.authors?.map(
-      (author) =>
-        author?.slug && (
-          <li key={author.slug}>
-            <Link to={`/authors/${author.slug}`}>{author.name}</Link>
-          </li>
-        )
-    )}
-    <p>
-      <Link to="/">Back to top page</Link>
-    </p>
-  </div>
-)
-// ______________________________________________________
-//
-export const pageQuery = graphql`
-  query Authors {
-    site {
-      siteMetadata {
-        authors {
-          name
-          slug
+const Component: React.FC = () => {
+  const data: AuthorsQuery = useStaticQuery(graphql`
+    query Authors {
+      site {
+        siteMetadata {
+          authors {
+            name
+            slug
+          }
         }
       }
     }
-  }
-`
+  `)
+
+  return (
+    <div>
+      <h1>Authors</h1>
+      {data.site?.siteMetadata?.authors?.map(
+        (author) =>
+          author?.slug && (
+            <li key={author.slug}>
+              <Link to={`/authors/${author.slug}`}>{author.name}</Link>
+            </li>
+          )
+      )}
+      <p>
+        <Link to="/">Back to top page</Link>
+      </p>
+    </div>
+  )
+}
 // ______________________________________________________
 //
 export default Component
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,37 @@
 import * as React from 'react'
-import { Link, graphql } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import Layout from '../layouts/index'
 import { IndexPageQuery } from '../../types/graphql-types'
 // ______________________________________________________
 //
-interface IndexPageProps {
-  data: IndexPageQuery
-}
-// ______________________________________________________
-//
-const Component: React.FC<IndexPageProps> = ({ data }) => (
-  <Layout>
-    <div>
-      <h1>Hi people</h1>
-      <p>
-        Welcome to your new <strong>{data.site?.siteMetadata?.title}</strong>{' '}
-        site.
-      </p>
-      <p>Now go build something great.</p>
-      <Link to="/page-2/">Go to page 2</Link>
-      <p>
-        <Link to="/authors/">Go to authors</Link>
-      </p>
-    </div>
-  </Layout>
-)
-// ______________________________________________________
-//
-export const pageQuery = graphql`
-  query IndexPage {
-    site {
-      siteMetadata {
-        title
+const Component: React.FC = () => {
+  const data: IndexPageQuery = useStaticQuery(graphql`
+    query IndexPage {
+      site {
+        siteMetadata {
+          title
+        }
       }
     }
-  }
-`
+  `)
+
+  return (
+    <Layout>
+      <div>
+        <h1>Hi people</h1>
+        <p>
+          Welcome to your new <strong>{data.site?.siteMetadata?.title}</strong>{' '}
+          site.
+        </p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+        <p>
+          <Link to="/authors/">Go to authors</Link>
+        </p>
+      </div>
+    </Layout>
+  )
+}
 // ______________________________________________________
 //
 export default Component
